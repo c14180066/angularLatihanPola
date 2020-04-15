34f65a5cace9add8b2934bd8517a1161fc888eda
c14180066 <c14180066@john.petra.ac.id>
2020-04-15 09:55:49 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular';
-
-  show: boolean = true;
-  function(a,b){ 
+  a = 0;
+  b = 0;
+  data = 0;
+  temp = 3;
+  klikButton(dataA,dataB){
+    this.a = dataA;
+    this.b = dataB;
+    this.data = Math.pow(this.a,this.b)
+    this.temp = Math.pow(this.a,this.b);
+    this.temp = this.temp % 2;
+  };
+  clear(){
+    this.data = 0;
+    this.a = 0;
+    this.b = 0;
+    this.temp = 3;
   }
+  
 }
